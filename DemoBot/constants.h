427c9ae0754210a1dcbeb11a90f4146aa017d1a0
grassclipper <grassclipper@moeftc.org>
2015-08-04 21:28:06 -0400

    init commit

---
+++ DemoBot/constants.h
@@ -0,0 +1,9 @@
+
+const int MOTOR_POWER_DIVIDER = 2;
+const int JOYSTICK_LIMIT = 15;
+const int ARM_OPEN = 195;
+const int ARM_CLOSED = 20;
+const int CLAW_HIGH_LIMIT = 200;
+const int CLAW_LOW_LIMIT = 50;
+const int CLAW_SERVO_INIT = 75;
+const int ARM_SERVO_INIT = 7;
