c8caa69a7effac0cf6096c6de5945c12d1bd8ab1
DrSuess2 <truccmom@yahoo.com>
2015-08-06 12:05:43 -0400

    added the turn until see IR

--- DemoBot/autonomous.h
+++ DemoBot/autonomous.h
@@ -8,6 +8,10 @@ void runAutonomous() {
 	blockColor = SensorValue[colorSensor];
 	if(blockColor == (long)BLUECOLOR) {
 		(bool)irSeeker.mode = (irSeeker.mode == DSP_1200);
+		while(sensorValue(irSeeker) != 4) {
+			motor[rightMotor] = 50;
+			motor[leftMotor] = -50;
+		}
 	}
 	else if(blockColor == (long)YELLOWCOLOR) {
 		(bool)irSeeker.mode = (irSeeker.mode == DSP_600);
