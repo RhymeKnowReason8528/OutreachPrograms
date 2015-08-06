fd6d5e93fbf90c27b5a07159953f07fb8e54eac8
DrSuess2 <truccmom@yahoo.com>
2015-08-06 12:00:37 -0400

    even lower

--- DemoBot/constants.h
+++ DemoBot/constants.h
@@ -1,7 +1,7 @@
 
 const int MOTOR_POWER_DIVIDER = 2;
 const int JOYSTICK_LIMIT = 15;
-const int ARM_DOWN = 210;
+const int ARM_DOWN = 230;
 const int ARM_UP = 20;
 const int CLAW_HIGH_LIMIT = 90;
 const int CLAW_LOW_LIMIT = 50;
