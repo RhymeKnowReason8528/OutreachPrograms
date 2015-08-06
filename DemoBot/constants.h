a56b0089615b985d80964dec1b18fc1f2739830d
DrSuess2 <truccmom@yahoo.com>
2015-08-06 12:02:05 -0400

    made claw close tighter

--- DemoBot/constants.h
+++ DemoBot/constants.h
@@ -4,6 +4,6 @@ const int JOYSTICK_LIMIT = 15;
 const int ARM_DOWN = 230;
 const int ARM_UP = 20;
 const int CLAW_HIGH_LIMIT = 90;
-const int CLAW_LOW_LIMIT = 50;
+const int CLAW_LOW_LIMIT = 40;
 const int CLAW_SERVO_INIT = 90;
 const int ARM_SERVO_INIT = 75;
