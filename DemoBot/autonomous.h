b85c9d102ef0871161d3b5b200fbf4d61886f169
DrSuess2 <truccmom@yahoo.com>
2015-08-07 09:38:09 -0400

    changed structure and added comments
    The program used to ask what color the item in the claw is and then have
    a while loop for the touch sensor, so there were two while loops. I
    moved the code around a little so now it asked if the touch sensor is
    pressed, and THEN it asks what color the item in the claw is, this way
    there is only one while loop and less repeated code

--- DemoBot/autonomous.h
+++ DemoBot/autonomous.h
@@ -6,34 +6,35 @@ void runAutonomous() {
 	initSensor(&irSeeker, S3);
 
 	blockColor = SensorValue[colorSensor];
-	if(blockColor == (long)BLUECOLOR) {
-		(bool)irSeeker.mode = (irSeeker.mode == DSP_1200);
-		while(SensorValue(touchSensor) == 0) {
-			if(HTIRS2_AC_DIR == 5) {
+
+	while(SensorValue(touchSensor) == 0) {//while the touch sensor is not pressed
+		if(blockColor == (long)BLUECOLOR) {//if the item in claw is blue
+			(bool)irSeeker.mode = (irSeeker.mode == DSP_1200);//set the seeking mode to 1200 MHrtz
+			if(HTIRS2_AC_DIR == 5) {//if the IR beacon is in zone 5
+				//make the robot move forward
 				motor[rightMotor] = 50;
 				motor[leftMotor] = 50;
 			}
-			else if(HTIRS2_AC_DIR > 5) {
+			else if(HTIRS2_AC_DIR > 5) {//if the IR beacon is in a zone that is more that 5
+				motor[leftMotor] = 50;//give leftMotor 50% power
+			}
+			else if(HTIRS2_AC_DIR < 5) {//if the IR beacon is in a zone that is less than 5
+				motor[rightMotor] = 50;//give rightMotor 50% power
+			}//closing of else if
+		}//closing of if
+		else if(blockColor == (long)YELLOWCOLOR) {//if the item is yellow
+			(bool)irSeeker.mode = (irSeeker.mode == DSP_600);//set the seeking mode to 600 MHrtz
+			if(HTIRS2_AC_DIR == 5) {//if the IR beacon is in zone 5
+				//make the robot move forward
+				motor[rightMotor] = 50;
 				motor[leftMotor] = 50;
 			}
-			else if(HTIRS2_AC_DIR < 5) {
-				motor[rightMotor] = 50;
+			else if(HTIRS2_AC_DIR > 5) {//if the IR beacon is in a zone that is more than 5
+				motor[leftMotor] = 50;//give leftMotor 50% power
 			}
-		}
-	}
-	else if(blockColor == (long)YELLOWCOLOR) {
-		(bool)irSeeker.mode = (irSeeker.mode == DSP_600);
-		while(SensorValue(touchSensor) == 0) {
-			if(HTIRS2_AC_DIR == 5) {
-				motor[rightMotor] = 50;
-				motor[leftMotor] = 50;
-			}
-			else if(HTIRS2_AC_DIR > 5) {
-				motor[leftMotor] = 50;
-			}
-			else if(HTIRS2_AC_DIR < 5) {
-				motor[rightMotor] = 50;
-			}
-		}
-	}
-}
+			else if(HTIRS2_AC_DIR < 5) {//if the IR beacon is in a zone that is less than 5
+				motor[rightMotor] = 50;//give rightMotot 50% power
+			}//closing of else if
+		}//closing of else if
+	}//closing of while
+}//closing of function 'runAutonomous'
