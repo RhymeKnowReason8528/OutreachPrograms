fc7873dcd785d0603cdca2ba6c700c0a2b6308e2
DrSuess2 <truccmom@yahoo.com>
2015-08-07 18:30:59 -0400

    switched lots of things
    the arm position from up to down so it doesn't block the IR seeker, and
    the HTIRS2_AC_DIR (wrong command) to irSeeker.acDirection

--- DemoBot/autonomous.h
+++ DemoBot/autonomous.h
@@ -1,30 +1,30 @@
 
-int blockColor;
+long blockColor;
 
 void runAutonomous() {
 	tHTIRS2 irSeeker;//create a struct
 	initSensor(&irSeeker, S3);//initialize the IR sensor
 
 	servo[clawServo] = CLAW_CLOSED;//close the claw around the item
-	servo[armServo] = ARM_UP;//lift the arm up
+	servo[armServo] = ARM_DOWN;//lift the arm up
 
-	blockColor = SensorValue[colorSensor];//assign the color of the item in claw to blockColor
+	blockColor =SensorValue[colorSensor];//assign the color of the item in claw to blockColor
 
 	while(SensorValue(touchSensor) == 0) {//while the touch sensor is not pressed
 		if(blockColor == (long)BLUECOLOR) {//if the item in claw is blue
 			(bool)irSeeker.mode = (irSeeker.mode == DSP_1200);//set the seeking mode to 1200 MHrtz
-			if(HTIRS2_AC_DIR == 5) {//if the IR beacon is in zone 5
+			if(irSeeker.acDirection == 5) {//if the IR beacon is in zone 5
 				//make the robot move forward
 				motor[rightMotor] = 50;
 				motor[leftMotor] = 50;
 			}
-			else if(HTIRS2_AC_DIR > 5) {//if the IR beacon is in a zone that is more that 5
+			else if(irSeeker.acDirection < 5) {//if the IR beacon is in a zone that is more that 5
 				motor[leftMotor] = 50;//give leftMotor 50% power
 			}
-			else if(HTIRS2_AC_DIR < 5) {//if the IR beacon is in a zone that is less than 5
+			else if(irSeeker.acDirection > 5) {//if the IR beacon is in a zone that is less than 5
 				motor[rightMotor] = 50;//give rightMotor 50% power
 			}//closing of else if
-		}//closing of if
+		//}//closing of if
 		else if(blockColor == (long)YELLOWCOLOR) {//if the item is yellow
 			(bool)irSeeker.mode = (irSeeker.mode == DSP_600);//set the seeking mode to 600 MHrtz
 			if(HTIRS2_AC_DIR == 5) {//if the IR beacon is in zone 5
@@ -39,5 +39,6 @@ void runAutonomous() {
 				motor[rightMotor] = 50;//give rightMotot 50% power
 			}//closing of else if
 		}//closing of else if
+		readSensor(&irSeeker);
 	}//closing of while
 }//closing of function 'runAutonomous'
